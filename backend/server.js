3ec31068690ab9b510bdcb59254f6f5090cebd61
Sasha Hrabar <sasha.grabar.20.03.02@gmail.com>
2021-09-25 13:43:16 +0300

    change app.listen

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.use((err, req, res, next) => {
 
 // eslint-disable-next-line no-undef
 const port=process.env.PORT || 5000;
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log(`Server at http://localhost:${port}`);    
 });
