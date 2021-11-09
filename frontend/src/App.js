25607e1915d8ed215058bb6d3897f8a433b0bb2a
Sasha Hrabar <sasha.grabar.20.03.02@gmail.com>
2021-11-09 18:56:17 +0200

    added graphics, chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,9 @@ import { listProductCategories } from './actions/productActions';
 import LoadingBox from './components/LoadingBox';
 import MessageBox from './components/MessageBox';
 import MapScreen from './screens/MapScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import SupportScreen from './screens/SupportScreen';
+import ChatBox from './components/ChatBox';
 
 
 function App() {
@@ -134,6 +137,9 @@ function App() {
                   <li>
                     <Link to="/userlist">Users</Link>
                   </li>
+                  <li>
+                    <Link to="/support">Support</Link>
+                  </li>
                 </ul>
               </div>
             )}
@@ -230,6 +236,11 @@ function App() {
             path="/user/:id/edit"
             component={UserEditScreen}
           ></AdminRoute>
+          <AdminRoute
+            path="/dashboard"
+            component={DashboardScreen}
+          ></AdminRoute>
+          <AdminRoute path="/support" component={SupportScreen}></AdminRoute>
            <SellerRoute
             path="/productlist/seller"
             component={ProductListScreen}
@@ -240,7 +251,10 @@ function App() {
           ></SellerRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All right reserved</footer>
+        <footer className="row center">
+          {userInfo && !userInfo.isAdmin && <ChatBox userInfo={userInfo} />}
+          <div>All right reserved</div>{' '}
+        </footer>
       </div>
     </BrowserRouter>
   );
